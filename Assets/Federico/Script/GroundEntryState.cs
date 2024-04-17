cb7f278ae676bb211771bec80ca9f68097c2e11c
leonardo-pieraccioli <leonardo.pieraccioli@gmail.com>
2024-04-17 20:07:18 +0200

    [ADD] prototipo combo system
    Lavoro di Federico

---
+++ Assets/Federico/Script/GroundEntryState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundEntryState : MeleeBaseState
+{
+    public override void OnEnter(StateMachine _stateMachine)
+    {
+        base.OnEnter(_stateMachine);
+
+        //Attack
+        attackIndex = 1;
+        duration = 0.4f;
+        
+        animator.SetTrigger("Attack" + attackIndex);
+        Debug.Log("Player Attack " + attackIndex + " Fired!");
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+        
+        if (fixedtime >= duration)
+        {
+           
+            if (shouldCombo)
+            {
+                animator.ResetTrigger("Attack"+attackIndex);
+                stateMachine.SetNextState(new GroundComboState());
+            }
+            else
+            {
+                animator.ResetTrigger("Attack"+attackIndex);
+                stateMachine.SetNextStateToMain();
+            }
+        }
+    }
+}
