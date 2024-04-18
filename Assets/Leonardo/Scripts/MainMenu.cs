42892a5e1fce689c4115b02704dd983b12a0f27e
Leonardo Pieraccioli <leonardo.pieraccioli@gmail.com>
2024-04-18 11:31:54 +0200

    [ADD] Schermata iniziale con collegamenti principali
    Commands, Credits finite, Options in progress

---
+++ Assets/Leonardo/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //[SerializeField] private GameObject transitionCanvas;
+
+    public void GoToLevelSelector()
+    {
+        //transitionCanvas.GetComponent<LevelLoader>().LoadNextLevel(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
