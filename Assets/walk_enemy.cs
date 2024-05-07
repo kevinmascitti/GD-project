8ae12cdc0721e600bca5b2e88de9458a1d17302d
Gioeleramondetti5 <gioeleramondetti20@gmail.com>
2024-05-08 01:02:05 +0200

    [new] animazioni e script per trailer

---
+++ Assets/walk_enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk_enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool flag=false;
+    public float speed = 3f;
+    void Start()
+    {
+        flag = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (flag)
+        {
+            float translation = speed * Time.deltaTime;
+            // Sposta l'oggetto lungo l'asse X
+            transform.Translate(-translation, 0, 0);
+        }
+        
+    }
+
+}
