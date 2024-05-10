730519c38a297f372aaa23fb503ad3f83fa17be5
Federico <s306156@studenti.polito.it>
2024-05-10 11:38:47 +0200

    [NEW] controller personale fede

--- Assets/Gioele/Scripts/SimpleThirdPersonController.cs
+++ Assets/Gioele/Scripts/SimpleThirdPersonController.cs
@@ -35,27 +35,31 @@ public class SimpleThirdPersonController : MonoBehaviour
             PlayerAnim.SetFloat("horizontal", h);
             PlayerAnim.SetFloat("vertical", v);
         }
-
+        
         if (h>0 || v>0)
         {
             GetComponent<Animator>().SetBool("walking", true);
         }
+        
+        
         _inputVector = new Vector3(h, 0, v);
         _inputSpeed = Mathf.Clamp(_inputVector.magnitude, 0f, 1f);
 
         //Compute direction According to Camera Orientation
         _targetDirection = Camera.transform.TransformDirection(_inputVector).normalized;
         _targetDirection.y = 0f;
-
+        
         if (_inputSpeed <= 0f)
         {
             GetComponent<Animator>().SetBool("walking", false);
-            return;
 
-        //Calculate the new expected direction (newDir) and rotate
-        Vector3 newDir = Vector3.RotateTowards(transform.forward, _targetDirection, RotationSpeed * Time.deltaTime, 0f);
-        transform.rotation = Quaternion.LookRotation(newDir);
-      
+            // COMMENTATO DA FEDE PERCHè DAVA ERRORE 
+            //Calculate the new expected direction (newDir) and rotate
+            Vector3 newDir =
+                Vector3.RotateTowards(transform.forward, _targetDirection, RotationSpeed * Time.deltaTime, 0f);
+            transform.rotation = Quaternion.LookRotation(newDir);
+        }
+        
         //Translate along forward
         transform.Translate(transform.forward * _inputSpeed * Speed * Time.deltaTime, Space.World);
         if (fede)
@@ -64,22 +68,31 @@ public class SimpleThirdPersonController : MonoBehaviour
                 Vector3.Dot(transform.forward,
                     new Vector3 (_inputSpeed * Speed, _inputSpeed * Speed, _inputSpeed * Speed)));
         }
+        
         if (transform.position.z < depthPoint.transform.position.z - minDepthBound)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, depthPoint.transform.position.z - minDepthBound);
         }
         else
         {
-            
+           
             //Calculate the new expected direction (newDir) and rotate
             Vector3 newDir =
                 Vector3.RotateTowards(transform.forward, _targetDirection, RotationSpeed * Time.deltaTime, 0f);
             transform.rotation = Quaternion.LookRotation(newDir);
-
+            
             //Translate along forward
             transform.Translate(transform.forward * _inputSpeed * Speed * Time.deltaTime, Space.World);
+            if (fede)
+            {
+                PlayerAnim.SetFloat("forward",
+                    Vector3.Dot(transform.forward,
+                        new Vector3 (_inputSpeed * Speed, _inputSpeed * Speed, _inputSpeed * Speed)));
+            }
+            
             if (depthPoint && transform.position.z < depthPoint.transform.position.z - minDepthBound)
             {
+                
                 transform.position = new Vector3(transform.position.x, transform.position.y,
                     depthPoint.transform.position.z - minDepthBound);
             }
