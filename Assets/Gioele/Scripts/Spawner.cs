366c3b990ae2edb8eaeeff46eeca4ad709d899f2
Gioeleramondetti5 <gioeleramondetti20@gmail.com>
2024-05-14 17:00:09 +0200

    [fix] modifiche per spawner

--- Assets/Gioele/Scripts/Spawner.cs
+++ Assets/Gioele/Scripts/Spawner.cs
@@ -1,40 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
     public List<GameObject> spawnableObjects;
+    public List<GameObject> EnemyObjects;
     public List<float> spawnProbability;
-    private float totalProbability=0f;
+    [NonSerialized]private float _totalProbability=0f;
     public GameObject plane;
     public float spawnTime = 2f;
+    public bool canSpawn = true;
     void Start()
     {
         //somma delle probabilità totali
-        totalProbability = 0f;
+        _totalProbability = 0f;
         for (int i=0;i<spawnableObjects.Count;i++)
         {
-            totalProbability += spawnProbability[i];
+            _totalProbability += spawnProbability[i];
         }
-        
         //start spawn
+        StartSpawn();
+        //setto i nemici del primo livello
+        ChangePrefabs(0);
         StartCoroutine(SpawnObjects());
     }
 
-    public void changeSpawnTime(float time)
+    public void ChangeSpawnTime(float time)
     {
         spawnTime = time;
+    }
+    public void StopSpawn()
+    {
+        // se è false non spawna nulla
+        canSpawn = false;
+    }
+    public void StartSpawn()
+    {
+        canSpawn = true;
+    }
+
+    public void ChangePrefabs(int indice)
+    {
+        // immagino gia di avere una lista con il totale dei  3 nemici di ogni livello nella lista di prefabs
+        
+        switch (indice)
+        {
+            case 0:
+                spawnableObjects[0] = EnemyObjects[0];
+                spawnableObjects[1] = EnemyObjects[1];
+                spawnableObjects[2] = EnemyObjects[2];
+                break;
+            case 1:
+                spawnableObjects[0] = EnemyObjects[3];
+                spawnableObjects[1] = EnemyObjects[4];
+                spawnableObjects[2] = EnemyObjects[5];
+                break;
+            case 2:
+                spawnableObjects[0] = EnemyObjects[6];
+                spawnableObjects[1] = EnemyObjects[7];
+                spawnableObjects[2] = EnemyObjects[8];
+                break;
+        }
     }
 
     IEnumerator SpawnObjects()
     {
-        while (true)
+        while (true && canSpawn)
         {
             yield return new WaitForSeconds(spawnTime); //  2 secondi--> da decidere/ modificare , Fede divertiti :)
 
             // Generiamo un numero casuale per selezionare l'oggetto da spawnare
-            float randomValue = Random.Range(0f, totalProbability);
+            float randomValue = Random.Range(0f, _totalProbability);
 
             // Troviamo l'oggetto da spawnare in base alla probabilità
             float cumulativeProbability = 0f;
