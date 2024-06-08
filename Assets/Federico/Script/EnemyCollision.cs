a4adf8ac46096f8be5681161e7e96be9abfc0029
Federico <s306156@studenti.polito.it>
2024-06-08 17:24:49 +0200

    [fix] fix danni multipli al muro non ancora terminato

--- Assets/Federico/Script/EnemyCollision.cs
+++ Assets/Federico/Script/EnemyCollision.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyCollision : MonoBehaviour
@@ -8,63 +7,57 @@ public class EnemyCollision : MonoBehaviour
     public GameObject player;
 
     [SerializeField] private int comboValue;
-
     public static EventHandler<EnemyCollisionArgs> OnAttackLended;
     private GameObject hitEffectPrefabTemp;
-
     public GameObject HitEffectPrefab;
-    /*
-    public void OnCollisionEnter(Collision other)
-    {
-        Enemy enemy = other.gameObject.GetComponent<Enemy>();
-        Animator anim = player.GetComponent<Animator>();
-        Vector3 contactPoint = other.collider.ClosestPoint(this.transform.position);
-      
-        
-        if (enemy != null && anim.GetFloat("Weapon.Active") > 0f)
-        {
-            // enemy.TakeDamage(20000.0f);        
-            hitEffectPrefabTemp = GameObject.Instantiate(HitEffectPrefab, contactPoint, Quaternion.identity);
-            StartCoroutine("KillHitEffect");
-            
-            // StartCoroutine("KillHitEffect");
-            Debug.Log("Enemy Hit");
-            OnAttackLended?.Invoke(this, new EnemyCollisionArgs(comboValue));
-        }
-    }
-    */
+
+    private bool canCollide = true;
+    private float collisionCooldown = 0.1f; // Cooldown di 0.1 secondi
+
     public void OnTriggerEnter(Collider other)
     {
+        Debug.Log("OnTriggerEnter called with: " + other.name);
+        /*
+        if (!canCollide) 
+        {
+            Debug.Log("Collision ignored due to cooldown.");
+            return;
+        }
+        */
+        
+
         Enemy enemy = other.gameObject.GetComponent<Enemy>();
         Animator anim = player.GetComponent<Animator>();
         Vector3 contactPoint = other.ClosestPoint(this.transform.position);
-    
+
         if (enemy != null && anim.GetFloat("Weapon.Active") > 0f)
         {
-            // Calcola la posizione corretta per l'effetto
-            Vector3 effectPosition = other.transform.position + new Vector3(0, +1.5f, 0); // Esempio di offset verticale
-        
+            Vector3 effectPosition = other.transform.position + new Vector3(0, +1.5f, 0);
             hitEffectPrefabTemp = GameObject.Instantiate(HitEffectPrefab, effectPosition, Quaternion.identity);
             StartCoroutine(KillHitEffect(hitEffectPrefabTemp));
             Debug.Log("Enemy Hit");
             OnAttackLended?.Invoke(this, new EnemyCollisionArgs(comboValue));
+
+            canCollide = false; // Disabilita ulteriori collisioni per la durata del cooldown
+            StartCoroutine(CollisionCooldown());
         }
 
         enemy = null;
     }
 
-
-   
+    IEnumerator CollisionCooldown()
+    {
+        yield return new WaitForSeconds(collisionCooldown);
+        canCollide = true; // Riabilita le collisioni dopo il cooldown
+    }
 
     IEnumerator KillHitEffect(GameObject hitEffect)
     {
         yield return new WaitForSeconds(0.2f);
-
         GameObject.Destroy(hitEffect);
     }
-
-    
 }
+
 public class EnemyCollisionArgs : EventArgs
 {
     public EnemyCollisionArgs(int a)
@@ -74,4 +67,3 @@ public class EnemyCollisionArgs : EventArgs
 
     public int comboValue;
 }
-
