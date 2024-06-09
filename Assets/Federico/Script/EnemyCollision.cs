4a723b9ec9bbf81ff117d70f53ea4d046c0bca59
Federico <s306156@studenti.polito.it>
2024-06-09 11:45:30 +0200

    [NEW] bug fixing del combact system

--- Assets/Federico/Script/EnemyCollision.cs
+++ Assets/Federico/Script/EnemyCollision.cs
@@ -14,23 +14,46 @@ public class EnemyCollision : MonoBehaviour
     private bool canCollide = true;
     private float collisionCooldown = 0.1f; // Cooldown di 0.1 secondi
 
+    public Boolean CheckCollision(Enemy Enemy,Animator PlayerAnim)
+    {
+        if (Enemy == null)
+            return false;
+        if (
+            (PlayerAnim.GetFloat("LeftArm")>0.0 &&
+             PlayerAnim.GetFloat("RightArm")==0.0 && 
+             PlayerAnim.GetFloat("LeftKick")==0.0) || 
+            (PlayerAnim.GetFloat("LeftArm")==0.0 &&
+            PlayerAnim.GetFloat("RightArm")>0.0 &&
+            PlayerAnim.GetFloat("LeftKick")==0.0) ||
+            (PlayerAnim.GetFloat("LeftArm")==0.0 &&
+             PlayerAnim.GetFloat("RightArm")==0.0 &&
+             PlayerAnim.GetFloat("LeftKick")>0.0)
+            )
+        {
+            return true;
+        }
+        
+       // Debug.Log("Error EnemyCollision.cs end of if reached without any return");
+        return false; 
+    }
     public void OnTriggerEnter(Collider other)
     {
         Debug.Log("OnTriggerEnter called with: " + other.name);
-        /*
+        
         if (!canCollide) 
         {
             Debug.Log("Collision ignored due to cooldown.");
             return;
         }
-        */
+        
         
 
         Enemy enemy = other.gameObject.GetComponent<Enemy>();
         Animator anim = player.GetComponent<Animator>();
         Vector3 contactPoint = other.ClosestPoint(this.transform.position);
-
-        if (enemy != null && anim.GetFloat("Weapon.Active") > 0f)
+        // sostiuire questa riga se l'animator da errore :)
+        //enemy!=null && anim.GetFloat("Weapon.Active")>0.0
+        if (CheckCollision(enemy,anim))
         {
             Vector3 effectPosition = other.transform.position + new Vector3(0, +1.5f, 0);
             hitEffectPrefabTemp = GameObject.Instantiate(HitEffectPrefab, effectPosition, Quaternion.identity);
