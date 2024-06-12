be5b1e35b00a9802c89485fc46858b5cc50309f0
Gioeleramondetti5 <gioeleramondetti20@gmail.com>
2024-06-13 01:52:13 +0200

    [fix] reduce damage

--- Assets/Kevin/Scripts/Character.cs
+++ Assets/Kevin/Scripts/Character.cs
@@ -9,7 +9,7 @@ public abstract class Character : MonoBehaviour
     public float currentHP;
     public bool isPlayer = false;
     public int atk;
-    public float damageReducer;
+    public float damageReducer=1;
 
     // Update is called once per frame
     public void Update()
