2be6de08885288831ef3328b776fa70e0d5dea7d
leonardo-pieraccioli <leonardo.pieraccioli@gmail.com>
2024-06-13 22:45:29 +0200

    [ADD] Main menu and game over menu on main scene

--- Assets/Leonardo/Scripts/MainMenu.cs
+++ Assets/Leonardo/Scripts/MainMenu.cs
@@ -6,11 +6,10 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     //[SerializeField] private GameObject transitionCanvas;
-
     public void GoToLevelSelector()
     {
         //transitionCanvas.GetComponent<LevelLoader>().LoadNextLevel(SceneManager.GetActiveScene().buildIndex - 1);
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
 
     public void QuitGame()
