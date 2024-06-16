8d91504262525151ef6c33564e9ceec6731e48f2
chiara.sanfilippo <chiara.sanfilippo@gmail.com>
2024-06-16 16:54:00 +0200

    feat: main menu UI

--- Assets/Chiara/Script/ButtonHover.cs
+++ Assets/Chiara/Script/ButtonHover.cs
@@ -1,23 +1,35 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public Vector3 hoverScale = new Vector3(1.1f, 1.1f, 1.1f); // Scale to increase to
+    public Vector3 hoverScale = new Vector3(1.05f, 1.05f, 1.05f); // Scale to increase to
     public float rotationAmount = 5f; // Rotation amount for the wobble effect
     public float rotationSpeed = 5f; // Speed of the rotation
     public float transitionDuration = 0.2f; // Duration for scaling transition
+
+    public Sprite selectedImage;
+    public Sprite unselectedImage;
+
 
     private Vector3 originalScale;
     private Quaternion originalRotation;
     private bool isHovered = false;
     private Coroutine transitionCoroutine;
 
+
+
     void Start()
     {
         originalScale = transform.localScale;
         originalRotation = transform.rotation;
+        changeToUnselected();
+
+        GetComponent<Button>().onClick.AddListener(delegate { backToNormal(); });
+
     }
 
     void Update()
@@ -38,6 +50,7 @@ public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerEx
             StopCoroutine(transitionCoroutine);
         }
         transitionCoroutine = StartCoroutine(ScaleTo(hoverScale, transitionDuration));
+        changeToSelected();
     }
 
     public void OnPointerExit(PointerEventData eventData)
@@ -49,6 +62,27 @@ public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerEx
         }
         transitionCoroutine = StartCoroutine(ScaleTo(originalScale, transitionDuration));
         StartCoroutine(RotateTo(originalRotation, transitionDuration));
+        changeToUnselected();
+    }
+
+    private void backToNormal()
+    {
+        isHovered = false;
+        transform.localScale = originalScale;
+        transform.rotation = Quaternion.identity;
+        changeToUnselected();
+    }
+
+    private void changeToSelected()
+    {
+        var image = GetComponent<Image>();
+        image.sprite = selectedImage;
+    }
+
+    private void changeToUnselected()
+    {
+        var image = GetComponent<Image>();
+        image.sprite = unselectedImage;
     }
 
     private IEnumerator ScaleTo(Vector3 targetScale, float duration)
