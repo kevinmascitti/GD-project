dcb7c905f5a471ca204e06d64eb7f3d39c70c2ab
leonardo-pieraccioli <leonardo.pieraccioli@gmail.com>
2024-06-18 10:07:18 +0200

    [UPDATE] first implementation of ad video playing

--- Assets/Leonardo/Scripts/PowerUp/LifeUpPU.cs
+++ Assets/Leonardo/Scripts/PowerUp/LifeUpPU.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 using UnityEngine.Timeline;
 
 public class LifeUpPU : PowerUp {
+    private PlayerCharacter playerCharacter;
+
     public void Start() {
+        playerCharacter = GameObject.Find("Player").GetComponent<PlayerCharacter>();
         PUname = "LifeUp";
         PUduration = -1;
         PUvalue = 10;
@@ -13,6 +16,7 @@ public class LifeUpPU : PowerUp {
     public override void ActivatePowerUp(object sender, EventArgs args)
     {
         Debug.Log("LifeUp attivato");
+        playerCharacter.UpdateHP(PUvalue);
         PowerUpManager.DisablePowerUp();
     }
 }
