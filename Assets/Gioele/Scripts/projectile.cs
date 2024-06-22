baf5153d9e9c97f570771b06a1bf64d57779449b
Gioeleramondetti5 <gioeleramondetti20@gmail.com>
2024-06-22 16:02:28 +0200

    [fix] modifica danno proiettile

--- Assets/Gioele/Scripts/projectile.cs
+++ Assets/Gioele/Scripts/projectile.cs
@@ -7,8 +7,6 @@ public class projectile : MonoBehaviour
 {
     public int damage = 2;
     public float destroyTime = 5.0f;
-    public int sign;
-    public bool barrel;
     private Vector3 direction_player;
     public void Start()
     {
@@ -24,7 +22,7 @@ public class projectile : MonoBehaviour
         {
             // il danno del projectile è damage e lo sottraggo alla vita del player
             // vado a diminuire la vita al player che viene colpito 
-            other.GetComponent<PlayerCharacter>().TakeDamage(5);
+            other.GetComponent<PlayerCharacter>().TakeDamage(damage);
             Destroy(gameObject);
         }
         
@@ -40,11 +38,8 @@ public class projectile : MonoBehaviour
 
     private void Update()
     {
-        // fino a che non collide
-        // sign mi da la direzione corretta di shooting 
-        
+        // spara nella direzione del player
         transform.position += new Vector3(direction_player.x,0,direction_player.z) * 3.5f * Time.deltaTime;
         
-        // devo inserire la rotazione
     }
 }
